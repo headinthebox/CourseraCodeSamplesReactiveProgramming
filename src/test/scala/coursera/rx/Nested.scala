c0c55a237a26bd55b588630b52ee056507670a52
samuelgruetter <samuel.gruetter@epfl.ch>
2013-11-25 00:02:21 +0100

    clarify that flatten behaves non-deterministically

--- src/test/scala/coursera/rx/Nested.scala
+++ src/test/scala/coursera/rx/Nested.scala
@@ -8,14 +8,20 @@ import org.scalatest.junit.JUnitSuite
 import org.junit.Assert._
 
 class Nested extends JUnitSuite {
-
+ 
   @Test def flattenNestedStreams(): Unit = {
 
     val xs: Observable[Int]              = Observable(3,2,1)
     val yss: Observable[Observable[Int]] = xs.map(x => Observable.interval(x seconds).map(_ => x).take(2))
     val zs = yss.flatten
 
-    assertEquals(List(1, 2, 1, 3, 2, 3), zs.toBlockingObservable.toList)
+    val list = zs.toBlockingObservable.toList
+    val isFirst = list == List(1, 1, 2, 3, 2, 3)
+    val isSecond = list == List(1, 2, 1, 3, 2, 3) 
+    // behavior of flatten is non-deterministic
+    assertTrue(isFirst || isSecond)
+    if (isFirst) println("first option")
+    if (isSecond) println("second option")
   }
 
   @Test def concatenateNestedStreams(): Unit = {
