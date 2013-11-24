209b84ae7682abe495f0fe27687cd31d142ec5bc
headinthebox <emeijer@applied-duality.com>
2013-11-24 00:35:19 -0800

    Creation

--- src/Coursera/Rx/Creation.scala
+++ src/Coursera/Rx/Creation.scala
@@ -9,8 +9,85 @@ object Creation {
      Observable(observer => {
        source.foreach(observer.onNext(_))
        observer.onCompleted()
+
+       // When you return an empty subscription
+       // the alarm bells should go off.
        Subscription{}
      })
   }
 
+  def never[T]: Observable[T] = {
+    // except here
+    Observable(observer => Subscription{})
+  }
+
+  def empty[T](): Observable[T] = {
+    Observable(observer => {
+      // here
+      observer.onCompleted()
+      Subscription{}
+    })
+  }
+
+  def error[T](error: Throwable): Observable[T] = {
+    Observable(observer => {
+      // here
+      observer.onError(error)
+      Subscription{}
+    })
+  }
+
+  def single[T](value: T): Observable[T] = {
+    Observable(observer => {
+      // and here
+      observer.onNext(value)
+      Subscription{}
+    })
+  }
+
+  def startWith[T](source: Observable[T], values: T*): Observable[T] = {
+    Observable(observer => {
+      values.foreach(observer.onNext)
+      source.subscribe(observer)
+    })
+  }
+
+  def filter[T](source: Observable[T], predicate: T=>Boolean): Observable[T] = {
+    Observable(observer => {
+      source.subscribe(
+        value => if(predicate(value)) observer.onNext(value),
+        error => observer.onError(error),
+        () => observer.onCompleted
+      )
+    })
+  }
+
+  def map[T, R](source: Observable[T], selector: T=>R): Observable[R] = {
+    Observable(observer => {
+      source.subscribe(
+        value => observer.onNext(selector(value)),
+        error => observer.onError(error),
+        () => observer.onCompleted
+      )
+    })
+  }
+
+  def map[T,R](source: Iterable[T], selector: T=>R): Iterable[R] = {
+    new Iterable[R] {
+
+      def iterator = new Iterator[R] {
+
+        // should grab outer here.
+        val outer: Iterator[T] = source.iterator
+
+        def hasNext: Boolean = {
+          outer.hasNext
+        }
+
+        def next(): R = {
+          selector(outer.next())
+        }
+      }
+    }
+  }
 }
