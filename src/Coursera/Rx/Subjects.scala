8212298205761d618b92a5310a3a394567f885f9
headinthebox <emeijer@applied-duality.com>
2013-11-24 12:29:26 -0800

    Subjects

--- src/Coursera/Rx/Subjects.scala
+++ src/Coursera/Rx/Subjects.scala
@@ -1,6 +1,6 @@
 package coursera.rx
 
-import rx.lang.scala.subjects.{AsyncSubject, ReplaySubject, PublishSubject}
+import rx.lang.scala.subjects.{BehaviorSubject, AsyncSubject, ReplaySubject, PublishSubject}
 
 object Subjects {
 
@@ -45,6 +45,36 @@ object Subjects {
   def ReplaySubjectIsAChannel() {
 
     val channel = ReplaySubject[Integer]()
+    val a = channel.subscribe(x => println(s"Apple: $x"), e => println(s"Apple~ $e"), () => println(s"Apple."))
+    val b = channel.subscribe(x => println(s"Banana: $x"), e => println(s"Banana~ $e"), () => println(s"Banana."))
+
+    channel.onNext(42)
+
+    a.unsubscribe()
+
+    channel.onNext(4711)
+    channel.onCompleted()
+
+    val c = channel.subscribe(x => println(s"Cranberry: $x"), e => println(s"Cranberry~ $e"), () => println(s"Cranberry."))
+
+    channel.onNext(13)
+  }
+
+  /*
+  Apple: 2013
+  Banana: 2013
+
+  Banana: 42
+  Apple: 42
+
+  Banana: 4711
+  Banana.
+
+  Cranberry.
+   */
+  def BehaviorSubjectIsACache() {
+
+    val channel = BehaviorSubject(2013)
     val a = channel.subscribe(x => println(s"Apple: $x"), e => println(s"Apple~ $e"), () => println(s"Apple."))
     val b = channel.subscribe(x => println(s"Banana: $x"), e => println(s"Banana~ $e"), () => println(s"Banana."))
 
